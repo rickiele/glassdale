cfe0fb24b93a44004191ed8479e1184038c4e4c9
rickiele <rickienthefishies@gmail.com>
2020-12-03 15:58:41 -0600

    crimes show up in console.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 import { criminalList } from './criminals/criminalList.js'
 import { getCriminals } from './criminals/criminalProvider.js'
+import { convictionSelect } from './convictions/convictionSelect.js'
 
 criminalList();
 getCriminals();
+convictionSelect();
