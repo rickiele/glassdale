3041cf3111f73d29d0787c87c0cd7e8df94d8f27
rickiele <rickienthefishies@gmail.com>
2020-12-03 14:34:05 -0600

    shows on DOM, added some comments

--- scripts/criminals/criminalProvider.js
+++ scripts/criminals/criminalProvider.js
@@ -1,15 +1,31 @@
+// Empty array which will contain getCriminals function into - our fetch API function
 let criminals = []
 
+// Function which will contain a copy of the criminals array (.slice)
+// .slice makes this a copy of the array
+// No arguments in it because we want the whole thing
 export const useCriminals = () => {
     return criminals.slice();
 }
 
+// Get used to this because you will use this over and over again.
+// This function which will ask or fetch API from a source on the net. 
+// We are requesting data from an external source.
 export const getCriminals = () => {
+    // getCriminals is going to return a fetch API from https://criminals.glassdale.us/criminals
+    // https://criminals.glassdale.us/criminals server has to respond to our request
+    // We don't know how long it will take. The request is going out, being processed, and the data comes back.
     return fetch("https://criminals.glassdale.us/criminals")
+    // Process in place to wait for that data to come back 
+    // .then is a method, and inside is a function. .json is a method
         .then(response => response.json())
+        // another .then is chained onto it - which also contains a function
+        // what the previous .then has received (response)
+        // this .then will turn that data into an array
         .then(
             parsedCriminals => {
-                console.table(parsedCriminals)
+                // another way of displaying data
+                // console.table(parsedCriminals)
                 criminals = parsedCriminals
             }
         )
--- scripts/criminals/criminals.js
+++ scripts/criminals/criminals.js
@@ -1,10 +1,10 @@
-// criminals into HTML
+// Function that contains the HTML to be injected
 export const criminal = (criminalObj) => {
   return `
   <article class="criminal">
     <h2>${criminalObj.name}</h2>
     <div>Crime: ${criminalObj.conviction}</div>
-    <div>Term Start: ${new Date(criminal.incarceration.start).toLocaleDateString('en-US')}</div>
+    <div>Term Start: ${new Date(criminalObj.incarceration.start).toLocaleDateString('en-US')}</div>
     <div>Term End: ${new Date(criminalObj.incarceration.end).toLocaleDateString('en-US')}</div>
   </article>
   `
