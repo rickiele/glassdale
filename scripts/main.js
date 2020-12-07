9b5683c4e1a7e68b499fa6f1e749cbb2102f82d7
rickiele <rickienthefishies@gmail.com>
2020-12-07 10:20:17 -0600

    Select element is in, moving onto 4

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,24 @@
 import { criminalList } from './criminals/criminalList.js'
-import { getCriminals } from './criminals/criminalProvider.js'
 import { convictionSelect } from './convictions/convictionSelect.js'
 
 criminalList();
-getCriminals();
 convictionSelect();
+
+
+
+// TODO:
+// What feature are we implementing?
+// Filter our criminals by the crimes committed
+
+
+// What tasks do we need to complete to implement the feature?
+// filter the criminal data by matching the crime that has been selected
+
+// listen for the selection of a crime and capture the chosen value
+// Use the selected value to filter the criminal data
+// Update the DOM with the filtered criminal data
+
+
+// Which modules are involved?
+// criminalList
+// convictionSelect
